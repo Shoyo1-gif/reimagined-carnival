5d2578720511c9c2d6751b76e05747701eaa5600
Janish khira <119401981+Shoyo1-gif@users.noreply.github.com>
2023-09-17 22:19:09 +0000

    PreLab1

---
+++ PreLab1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class PreLab1 {
+    public static void main(String[] args){
+
+
+        Scanner myObj = new Scanner(System.in); //Created a scanner
+        System.out.println("Hello, how are you?"); //Asked a question
+
+        String Ans = myObj.nextLine(); //User input taken in
+        System.out.println(Ans );
+
+    }
+}

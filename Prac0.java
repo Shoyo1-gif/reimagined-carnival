d257ce747d906171fc18f84c369d3da2b4fad9a0
Janish khira <119401981+Shoyo1-gif@users.noreply.github.com>
2023-09-17 19:33:39 -0400

    Add files via upload

---
+++ Prac0.java
@@ -0,0 +1,23 @@
+public class Prac0 {
+    public static void main(String[] args){
+
+        int A = 2*3*4*5*6*7*8*9*10;
+        System.out.println("The product of first 10 positive integers: " + A);
+
+        int B = A*11*12*13*14*15*16*17;
+        System.out.println("The product of first 17 positive integers: " + B);
+
+        double x = 3.1;
+        double y = 5;
+        double z = 2/y + x*Math.sqrt(1/(9+Math.pow(y,2)));
+        System.out.println("Value of z: " + z);
+
+        double a = 0.000000001;
+        double w = (1/a) * a;
+
+        System.out.println("w: " + w);
+
+
+
+    }
+}

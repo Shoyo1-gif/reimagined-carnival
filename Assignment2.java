aa90ead567244e4b6a1c41ac89a6c70897127edb
Janish khira <119401981+Shoyo1-gif@users.noreply.github.com>
2023-09-21 10:11:55 -0400

    Add files via upload

---
+++ Assignment2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Assignment2 {
+    public static void main(String[] args){
+
+        Scanner newObj = new Scanner(System.in);
+
+        //System.out.println()
+
+
+        System.out.println ("Write a number: ");
+        int num = newObj.nextInt;
+        System.out.println(num);
+
+    }
+    
+}

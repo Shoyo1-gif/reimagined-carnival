0088d9464a184c9e63f9ed6f1fe4d6469e01f64b
Janish khira <119401981+Shoyo1-gif@users.noreply.github.com>
2024-01-24 04:20:04 +0000

    end

--- temp.java
+++ temp.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class temp{
     public static void main(String[] args){
-        
+        Scanner keyb = new Scanner(System.in);
+        System.out.println("Enter a temperature in Fahrenheit: ");
+        double f = keyb.nextDouble();
+
+        double c = (f - 32) * 5 / 9;
+        System.out.println("The temperature in Celsius is: " + c);
+
+        keyb.close();
         
     }
 }

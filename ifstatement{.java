08415c59ab1608ca170adce960fbef7bac5c55d8
Janish khira <119401981+Shoyo1-gif@users.noreply.github.com>
2024-01-29 18:18:29 +0000

    if statement

---
+++ ifstatement{.java
@@ -0,0 +1,10 @@
+public class ifstatement{
+    public static void main(String[] args){
+        int number = 10;
+        if(number<0){
+            System.out.println("T");
+        }
+
+        System.out.println("");
+    }
+}

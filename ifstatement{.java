c88ef9a243ebff1ff1244bba71b67662db8d6515
Janish khira <119401981+Shoyo1-gif@users.noreply.github.com>
2024-01-29 18:20:37 +0000

    end it

--- ifstatement{.java
+++ ifstatement{.java
@@ -2,9 +2,9 @@ public class ifstatement{
     public static void main(String[] args){
         int number = 10;
         if(number<0){
-            System.out.println("T");
+            System.out.println("The number is negative");
         }
 
-        System.out.println("");
+        System.out.println("Statement is outside the IF block");
     }
 }
